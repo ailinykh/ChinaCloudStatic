1b9bb6db612729dc8ab3998f1bf12add01e15b9a
Anthony Ilinykh <anthonyilinykh@gmail.com>
2016-10-25 16:46:48 +0300

    initial commit

---
+++ ChinaCloudStatic/ChinaCloudStatic.h
@@ -0,0 +1,24 @@
+//
+//  ChinaCloudStatic.h
+//  ChinaCloudStatic
+//
+//  Created by Anthony Ilinykh on 20/10/2016.
+//  Copyright © 2016 Anthony Ilinykh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "Rtsp_instance.h"
+#import "RtspInterface.h"
+#import "UDPManager.h"
+#import "Constants.h"
+#import "AccountResult.h"
+#import "LoginResult.h"
+#import "NetManager.h"
+#import "P2PCInterface.h"
+#import "P2PClient.h"
+#import "P2PMonitorController.h"
+
+@interface ChinaCloudStatic : NSObject
+
+@end
